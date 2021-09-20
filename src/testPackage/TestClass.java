99944f1b522920688386963cc8305b8d857d2b18
Asus <Asus@rakib-chp>
2021-09-21 00:47:15 +0600

    ki hosse esob, vallagena

---
+++ src/testPackage/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testPackage;
+
+/**
+ *
+ * @author Asus
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Jibon ta vaja vaja hoye gelo!!");
+    }
+}
